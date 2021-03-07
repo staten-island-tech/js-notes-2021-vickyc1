56a841ecb514289eb3c6ef0bc0bd7ea2992a5342
vickyc1 <71679303+vickyc1@users.noreply.github.com>
2021-03-07 11:16:01 -0800

    hi

---
+++ DOM Manipulation (Document Object Model)/dom.js
@@ -0,0 +1,5 @@
+//DOM (Document Object Model) is created by the browser when a web page is loaded.Looks like a tree/branches of elements/nodes, like parent elements on top and child elements beneath.
+//can use JS to read/change/interact with the DOM
+
+//querying/searching the DOM
+document;
